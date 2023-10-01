6ade6ba4deb5a8ffc29110926a59672865d2bceb
kelvinra <13521005@mahasiswa.itb.ac.id>
2023-10-01 18:19:45 +0700

    add detail meal page

---
+++ app/views/detailmeal/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../public/css/detailmeal.css">
+    <title>NutriCraft</title>
+</head>
+<body>
+    <div class="back">
+        <div class="content">
+            <div class="foodimage">
+                <img src="../../../assets/meal.jpg" alt="">
+                <div class="nutritionfacts">
+                    <h3>Nutrition Facts</h3>
+                    <div class="calories">
+                        <h5>300 cal</h5>
+                        <p>Calories</p>
+                    </div>
+                    <div class="carbs">
+                        <h5>20g</h5>
+                        <p>Carbs</p>
+                    </div>
+                    <div class="protein">
+                        <h5>20g</h5>
+                        <p>Protein</p>
+                    </div>
+                    <div class="fat">
+                        <h5>5g</h5>
+                        <p>Fat</p>
+                    </div>
+                    <div class="sugar">
+                        <h5>1g</h5>
+                        <p>Sugar</p>
+                    </div>
+                </div>
+            </div>
+            <div class="foodcontent">
+                <div class="foodtitle">
+                    <h1>Chicken Salad</h1>
+                    <h3>Lunch</h3>
+                </div>
+                <div class="fooddescription">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget molestie tellus. Nunc a euismod est. In finibus tellus et efficitur cursus...</p>
+                </div>
+                <div class="ingredients">
+                    <h3>Ingredients</h3>
+                    <ul>
+                        <li>Ingredient 1</li>
+                        <li>Ingredient 2</li>
+                        <li>Ingredient 3</li>
+                        <li>Ingredient 4</li>
+                        <li>Ingredient 5</li>
+                        <!-- Add more ingredients as needed -->
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ app/views/fact/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../public/css/fact.css">
+    <title>NutriCraft</title>
+</head>
+<body>
+    <div>
+        <div class="content">
+            <h1>NutriFact</h1>
+            <div class="header">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
